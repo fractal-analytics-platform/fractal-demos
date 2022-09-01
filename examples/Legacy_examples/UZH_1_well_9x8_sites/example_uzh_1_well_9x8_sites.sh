8e12784234b61b79468c6b49b4857d9adb66ae01
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2022-09-01 16:47:19 +0200

    Merge pull request #178 from fractal-analytics-platform/server
    Merge server into main

--- examples/Legacy_examples/UZH_1_well_9x8_sites/example_uzh_1_well_9x8_sites.sh
+++ examples/Legacy_examples/UZH_1_well_9x8_sites/example_uzh_1_well_9x8_sites.sh
@@ -5,12 +5,15 @@ PATH_INPUT=/data/active/fractal/3D/PelkmansLab/CardiacMultiplexing/Cycle1_9x8_si
 WFPARAMS=wf_params_uzh_1_well_9x8_sites.json
 
 MWE_DIR=/data/active/fractal/tests
-PATH_OUTPUT=${MWE_DIR}/Temporary_data_UZH_1_well_9x8_sites_multifov
+PATH_OUTPUT=${MWE_DIR}/Temporary_data_UZH_1_well_9x8_sites
 
-CMD='poetry run python ../fractal/fractal_cmd.py'
+CMD='poetry run python ../../fractal/fractal_cmd.py'
 
 #echo 'Re-install poetry'
 #poetry install
+
+date
+echo
 
 echo 'Clean up'
 rm -rf $PATH_OUTPUT
@@ -30,63 +33,80 @@ $CMD dataset update-type mwe-test dstest png
 $CMD dataset list mwe-test
 echo
 
-echo 'Add create_zarr_structure_multifov task'
-$CMD task add create_zarr_structure_multifov png zarr none
+echo 'Add create_zarr_structure task'
+$CMD task add create_zarr_structure png zarr none
 $CMD task list
 echo
 
-echo 'Add yokogawa_to_zarr_multifov task'
-$CMD task add yokogawa_to_zarr_multifov zarr zarr site
-$CMD task list
+echo 'Add yokogawa_to_zarr task'
+$CMD task add yokogawa_to_zarr zarr zarr well
 echo
 
 #echo 'Add replicate_zarr_structure'
 #$CMD task add replicate_zarr_structure zarr zarr plate
-$CMD task list
+#echo
+
+echo 'Add illumination_correction'
+$CMD task add illumination_correction zarr zarr well
 echo
 
-#echo 'Add illumination_correction'
-#$CMD task add illumination_correction zarr zarr well
-#$CMD task list
+echo 'Add replicate_zarr_structure_mip'
+$CMD task add replicate_zarr_structure_mip zarr zarr plate
 echo
 
-#echo 'Add replicate_zarr_structure_mip'
-#$CMD task add replicate_zarr_structure_mip zarr zarr plate
-#$CMD task list
+echo 'Add image_labeling'
+$CMD task add image_labeling zarr zarr well
+echo
+
+echo 'Add image_labeling_whole_well'
+$CMD task add image_labeling_whole_well zarr zarr well
 echo
 
 echo 'Add maximum_intensity_projection'
-$CMD task add maximum_intensity_projection zarr zarr site
+$CMD task add maximum_intensity_projection zarr zarr well
+echo
+
+echo
 $CMD task list
 echo
 
 #############################################
 
 echo 'Create workflow'
-$CMD workflow new mwe-test wftest create_zarr_structure_multifov
-$CMD workflow list mwe-test
+$CMD workflow new mwe-test wftest create_zarr_structure
 echo
 
-echo 'Add yokogawa_to_zarr_multifov task'
-$CMD workflow add-task mwe-test wftest yokogawa_to_zarr_multifov
-$CMD workflow list mwe-test
+echo 'Add yokogawa_to_zarr task'
+$CMD workflow add-task mwe-test wftest yokogawa_to_zarr
+echo
+
+echo 'Add illumination_correction'
+$CMD workflow add-task mwe-test wftest illumination_correction
+echo
+
+echo 'Add image_labeling'
+$CMD workflow add-task mwe-test wftest image_labeling
 echo
 
 echo 'Add replicate_zarr_structure_mip'
 $CMD workflow add-task mwe-test wftest replicate_zarr_structure_mip
-$CMD workflow list mwe-test
 echo
-
-#echo 'Add illumination_correction'
-#$CMD workflow add-task mwe-test wftest illumination_correction
-#$CMD workflow list mwe-test
-#echo
 
 echo 'Add maximum_intensity_projection'
 $CMD workflow add-task mwe-test wftest maximum_intensity_projection
+echo
+
+echo 'Add image_labeling_whole_well'
+$CMD workflow add-task mwe-test wftest image_labeling_whole_well
+echo
+
+echo 'Final list:'
 $CMD workflow list mwe-test
 echo
 
 echo 'Execute workflow'
 $CMD workflow apply mwe-test wftest dstest dstest $PATH_INPUT $PATH_OUTPUT $WFPARAMS
 echo
+
+echo
+date
