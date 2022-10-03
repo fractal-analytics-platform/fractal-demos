c40b466b977b391c03125d14a2af13ac40f94053
jluethi <joel.luethi@uzh.ch>
2022-10-03 11:08:15 +0200

    Update run file to run in parallel

--- demo-october-2022/example_2x2_cardio_fmi/aux_extract_from_simple_json.py
+++ demo-october-2022/example_2x2_cardio_fmi/aux_extract_from_simple_json.py
@@ -2,17 +2,25 @@ import json
 import sys
 
 
-filename = sys.argv[1]
-keys = sys.argv[2:]
+assert len(sys.argv[1:]) > 1
+
+filename, which_id = sys.argv[1:3]
+if which_id == "dataset_id":
+    assert len(sys.argv[1:]) == 3
+    dataset_name = sys.argv[3]
+
 with open(filename, "r") as fin:
     d = json.load(fin)
 
-for k in keys:
-    d = d[k]
-    if isinstance(d, list):
-        if len(d) > 1:
-            sys.exit("SOMETHING WENT WRONG")
-        else:
-            d = d[0]
-
-print(d)
+if which_id == "project_id":
+    # Safety check
+    assert "project_dir" in d.keys()
+    print(d["id"])
+elif which_id == "dataset_id":
+    dataset_list = d["dataset_list"]
+    dataset_id = next(ds["id"]
+                      for ds in dataset_list
+                      if ds["name"] == dataset_name)
+    print(dataset_id)
+else:
+    raise Exception("ERROR: {which_id=}")
