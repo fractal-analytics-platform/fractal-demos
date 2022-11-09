ab125bb42100a6b0f40884383401ec1f04fa3b77
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2022-11-09 08:32:13 +0000

    Handle FRACTAL_ROOT in server script

--- examples-v1/server/server_config_and_start.sh
+++ examples-v1/server/server_config_and_start.sh
@@ -12,10 +12,12 @@ alembic upgrade head
 # Remove old stuff
 rm -fr logs
 rm -fr runinfo
+rm -fr FRACTAL_ROOT
 rm cmd_parsl.slurm.*.*.sh
 
 # Set environment variables
-echo -e "DEPLOYMENT_TYPE=testing\nJWT_SECRET_KEY=secret\nDATA_DIR_ROOT=/tmp/\nSQLITE_PATH=./data/fractal_server.db\nFRACTAL_ROOT=/data/homes/fractal/fractal-server/fractal-demos/examples/server/FRACTAL_ROOT\nFRACTAL_LOGGING_LEVEL=10" > .fractal_server.env
+FRACTAL_ROOT=`pwd`/FRACTAL_ROOT
+echo -e "DEPLOYMENT_TYPE=testing\nJWT_SECRET_KEY=secret\nDATA_DIR_ROOT=/tmp/\nSQLITE_PATH=./data/fractal_server.db\nFRACTAL_ROOT=${FRACTAL_ROOT}\nFRACTAL_LOGGING_LEVEL=10" > .fractal_server.env
 
 
 # Start the server
