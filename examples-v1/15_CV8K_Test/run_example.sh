5bcac56d1bc415771284e097b01e5012adc87ef0
Joel Luethi <joel.luethi@uzh.ch>
2022-11-14 16:32:46 +0000

    Add CV8K test case

---
+++ examples-v1/15_CV8K_Test/run_example.sh
@@ -0,0 +1,93 @@
+LABEL="CV8K_15"
+
+###############################################################################
+# THINGS TO BE CHANGED BY THE USER
+# Adapt to settings to where you run the example
+BASE_FOLDER_EXAMPLE=`pwd`/..
+###############################################################################
+
+###############################################################################
+# IMPORTANT: modify the following lines, depending on your preferences
+# 1. They MUST include a `cd` command to a path where your user can write. The
+#    simplest is to use `cd $HOME`, but notice that this will create many sh
+#    scripts in your folder. You can also use `cd $HOME/fractal_parsl_scripts`,
+#    but first make sure that such folder exists
+# 2. They MAY include additional commands to load a python environment. The ones
+#    used in the current example are appropriate for the UZH setup.
+WORKER_INIT="\
+export HOME=$HOME; \
+mkdir -p $HOME/fractal_parsl_scripts; \
+cd $HOME/fractal_parsl_scripts; \
+"
+###############################################################################
+
+
+# Set useful variables
+PRJ_NAME="proj-$LABEL"
+DS_IN_NAME="input-ds-$LABEL"
+DS_OUT_NAME="output-ds-$LABEL"
+WF_NAME="Workflow $LABEL"
+
+# Set cache path and remove any previous file from there
+export FRACTAL_CACHE_PATH=`pwd`/".cache"
+rm -rv ${FRACTAL_CACHE_PATH}
+
+# Define/initialize empty project folder and temporary file
+PROJ_DIR=`pwd`/tmp_${LABEL}
+rm -r $PROJ_DIR
+mkdir $PROJ_DIR
+
+###############################################################################
+# IMPORTANT: modify the following lines so that they point to absolute paths
+INPUT_PATH="/data/active/fractal/Liberali/20221103_SFtestCV8000/CV8K_images"
+OUTPUT_PATH=${PROJ_DIR}/output
+###############################################################################
+
+# Create project
+OUTPUT=`fractal --batch project new $PRJ_NAME $PROJ_DIR`
+PRJ_ID=`echo $OUTPUT | cut -d ' ' -f1`
+DS_IN_ID=`echo $OUTPUT | cut -d ' ' -f2`
+echo "PRJ_ID: $PRJ_ID"
+echo "DS_IN_ID: $DS_IN_ID"
+
+# Update dataset name/type, and add a resource
+fractal dataset edit --name "$DS_IN_NAME" -t image --read-only $PRJ_ID $DS_IN_ID
+fractal dataset add-resource -g "*.tif" $PRJ_ID $DS_IN_ID $INPUT_PATH
+
+# Add output dataset, and add a resource to it
+DS_OUT_ID=`fractal --batch project add-dataset $PRJ_ID "$DS_OUT_NAME"`
+echo "DS_OUT_ID: $DS_IN_ID"
+
+fractal dataset edit -t zarr --read-write $PRJ_ID $DS_OUT_ID
+fractal dataset add-resource -g "*.zarr" $PRJ_ID $DS_OUT_ID $OUTPUT_PATH
+
+# Create workflow
+WF_ID=`fractal --batch workflow new "$WF_NAME" $PRJ_ID`
+echo "WF_ID: $WF_ID"
+
+# Add tasks to workflow
+# 1 -> create_zarr_structure
+# 2 -> yokogawa_to_zarr
+# 3 -> Replicate Zarr structure
+# 4 -> Maximum Intensity Projection
+# 5 -> Cellpose Segmentation
+# 6 -> Measurement
+# 7 -> Illumination correction
+fractal workflow add-task $WF_ID 1 --args-file Parameters/create_zarr_structure.json
+fractal workflow add-task $WF_ID 2
+fractal workflow add-task $WF_ID 3
+fractal workflow add-task $WF_ID 4
+fractal workflow add-task $WF_ID 5 --args-file Parameters/cellpose_segmentation.json
+fractal workflow add-task $WF_ID 6 --args-file Parameters/measurement.json
+
+# fractal workflow add-task $WF_ID "Replicate Zarr structure"
+# fractal workflow add-task $WF_ID "Maximum Intensity Projection"
+# echo "{\"labeling_level\": 3, \"executor\": \"cpu-low\"}" > ${PROJ_DIR}/args_labeling.json
+# fractal workflow add-task $WF_ID "Cellpose Segmentation" --args-file ${PROJ_DIR}/args_labeling.json --meta-file
+
+# Look at the current workflows
+fractal workflow show $WF_ID
+echo
+
+# Apply workflow
+fractal workflow apply -o $DS_OUT_ID -p $PRJ_ID $WF_ID $DS_IN_ID --worker-init "$WORKER_INIT"
