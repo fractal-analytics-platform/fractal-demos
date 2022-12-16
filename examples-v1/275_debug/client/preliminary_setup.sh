e624ed5378a75672b486b66095ad76029b0e02b0
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2022-12-16 13:15:25 +0000

    Update fake tasks package

--- examples-v1/275_debug/client/preliminary_setup.sh
+++ examples-v1/275_debug/client/preliminary_setup.sh
@@ -13,8 +13,8 @@ FRACTAL_SERVER=http://localhost:$PORT\
 fractal register -p $USERNAME ${USERNAME}@me.com $USERNAME
 
 # Trigger collection of core tasks
-#fractal task collect fractal-tasks-core --package-version 0.6.4
-fractal task collect `pwd`/package/fractal_tasks_core-0.6.6a1-py3-none-any.whl
+#fractal task collect fractal-tasks-core --package-version 0.6.5
+fractal task collect `pwd`/package/fractal_tasks_core-0.0.0-py3-none-any.whl
 
 echo "To quickly see whether task collection is complete, issue the command"
 echo "fractal task list"
