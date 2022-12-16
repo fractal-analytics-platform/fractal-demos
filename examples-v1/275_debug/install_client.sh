d0544102da35e3af106a0a0cfbde9cea7245306d
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2022-12-16 08:23:48 +0000

    Add examples-v1/275_debug

---
+++ examples-v1/275_debug/install_client.sh
@@ -0,0 +1,10 @@
+VERSION="1.0.0a6"
+
+ENVNAME=fractal-client-$VERSION
+conda deactivate
+conda remove --name $ENVNAME -y --all
+conda create --name $ENVNAME python==3.8.13 -y
+conda activate $ENVNAME
+conda update pip -y
+
+pip install fractal-client==$VERSION
