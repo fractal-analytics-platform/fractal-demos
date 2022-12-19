0dd9e02a2c2ffebbbe8effdcdf7f72db48e69c3c
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2022-12-19 10:03:28 +0000

    Remove 275_debug folder

--- examples-v1/275_debug/client/Activate_conda.sh
+++
@@ -1,2 +0,0 @@
-source /opt/easybuild/software/Anaconda3/2019.07/etc/profile.d/conda.sh
-conda activate fractal-client-1.0.0a6
--- examples-v1/275_debug/client/package/fractal-tasks-core-low/fractal_tasks_core/__init__.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-logging.basicConfig(
-    level=logging.INFO, format="%(asctime)s; %(levelname)s; %(message)s"
-)
-
-
-__VERSION__ = "0.0.0"
-__OME_NGFF_VERSION__ = "0.4"
