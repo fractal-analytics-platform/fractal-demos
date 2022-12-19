07d6c5a5330a60911b56ea1051673eee0ca4b4de
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2022-12-19 10:03:01 +0000

    Clean up client install script in example 01

--- examples-v1/01_cardio_tiny_dataset/install_client.sh
+++ examples-v1/01_cardio_tiny_dataset/install_client.sh
@@ -1,14 +1,10 @@
-ENVNAME=fractal-client-v1-a5
+VERSION="1.0.0a6"
+
+ENVNAME=fractal-client-$VERSION
 conda deactivate
 conda remove --name $ENVNAME -y --all
 conda create --name $ENVNAME python==3.8.13 -y
 conda activate $ENVNAME
 conda update pip -y
 
-
-VERSION="1.0.0a5"
 pip install fractal-client==$VERSION
-
-# Installing from github releases:
-# WHEEL=fractal_client-${VERSION}-py3-none-any.whl
-# pip install https://github.com/fractal-analytics-platform/fractal/releases/download/${VERSION}/${WHEEL}
