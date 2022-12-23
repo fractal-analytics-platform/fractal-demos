d0bec0dc8b11433f9ba14c9fbc775f28d305066a
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2022-12-23 10:13:56 +0100

    Cleanup server install script

--- examples-v1/server/install_script.sh
+++ examples-v1/server/install_script.sh
@@ -1,14 +1,10 @@
-ENVNAME=fractal-server-v1
+VERSION="1.0.0a9"
+
+ENVNAME=fractal-client-$VERSION
 conda deactivate
 conda remove --name $ENVNAME -y --all
 conda create --name $ENVNAME python==3.8.13 -y
 conda activate $ENVNAME
 conda update pip -y
 
-pip install fractal-server[slurm]==1.0.0b9
-
-# VERSION="1.0.0b4"
-# WHEEL=fractal_server-${VERSION}-py3-none-any.whl
-# wget https://github.com/fractal-analytics-platform/fractal-server/releases/download/${VERSION}/${WHEEL} .
-# pip install ${WHEEL}[slurm]
-# rm -v $WHEEL
+pip install fractal-server[slurm]==$VERSION
