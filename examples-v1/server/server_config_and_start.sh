f7766665e469f9d1492affa873194fbc0e240511
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-01-02 11:30:22 +0100

    Minor updates to server init script

--- examples-v1/server/server_config_and_start.sh
+++ examples-v1/server/server_config_and_start.sh
@@ -3,13 +3,9 @@
 PORT=8010
 
 # Create an empty db
-
-rm -fr data
-mkdir data
-export SQLITE_PATH=./data/fractal_server.db
+rm -fr db
+mkdir db
 fractalctl set-db
-#alembic revision --autogenerate
-#alembic upgrade head
 
 # Remove old stuff
 rm -fr FRACTAL_TASKS_DIR
@@ -23,11 +19,11 @@ FRACTAL_TASKS_DIR=`pwd`/FRACTAL_TASKS_DIR
 echo -e "\
 DEPLOYMENT_TYPE=testing
 JWT_SECRET_KEY=secret
-SQLITE_PATH=./data/fractal_server.db
+SQLITE_PATH=db/fractal_server.db
 FRACTAL_TASKS_DIR=${FRACTAL_TASKS_DIR}
 FRACTAL_LOGGING_LEVEL=10
 FRACTAL_RUNNER_BACKEND=slurm
-FRACTAL_RUNNER_WORKING_BASE_DIR=\"artifacts\"
+FRACTAL_RUNNER_WORKING_BASE_DIR=artifacts
 FRACTAL_SLURM_CONFIG_FILE=config_uzh.json
 " > .fractal_server.env
 
