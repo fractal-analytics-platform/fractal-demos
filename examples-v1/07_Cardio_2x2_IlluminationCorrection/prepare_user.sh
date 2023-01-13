46591440adbbbb97cffce48eefcfa1d9aee84e91
Joel Luethi <joel.luethi@uzh.ch>
2023-01-13 11:39:13 +0000

    Add illumination correction evaluation example

---
+++ examples-v1/07_Cardio_2x2_IlluminationCorrection/prepare_user.sh
@@ -0,0 +1,10 @@
+PORT=8002
+USERNAME="$(whoami)"
+echo -e "\
+FRACTAL_USER=${USERNAME}@me.com
+FRACTAL_PASSWORD=${USERNAME}
+SLURM_USER=${USERNAME}
+FRACTAL_SERVER=http://localhost:$PORT\
+" > .fractal.env
+
+#fractal register -p $USERNAME ${USERNAME}@me.com $USERNAME
