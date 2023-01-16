f158d6de3544ec8ef85d1c9b8b892584cf728363
Joel Luethi <joel.luethi@uzh.ch>
2023-01-16 10:05:19 +0000

    Update example v1 script

--- examples/01_cardio_tiny_dataset/run_example.sh
+++ examples/01_cardio_tiny_dataset/run_example.sh
@@ -1,17 +1,13 @@
 LABEL="1"
 
+# Initialization for some environment variables for the worker
+# Needed on clusters where users don't have write access to the conda env
 WORKER_INIT="\
 export CELLPOSE_LOCAL_MODELS_PATH=${HOME}/.cache/CELLPOSE_LOCAL_MODELS_PATH
 export NUMBA_CACHE_DIR=${HOME}/.cache/NUMBA_CACHE_DIR
 export NAPARI_CONFIG=${HOME}/.cache/NAPARI_CACHE_DIR
 export XDG_CONFIG_HOME=${HOME}/.cache/XDG
 "
-
-###############################################################################
-# THINGS TO BE CHANGED BY THE USER
-# Adapt to settings to where you run the example
-BASE_FOLDER_EXAMPLE=`pwd`/..
-###############################################################################
 
 # Set useful variables
 PRJ_NAME="proj-$LABEL"
@@ -21,16 +17,16 @@ WF_NAME="Workflow $LABEL"
 
 # Set cache path and remove any previous file from there
 export FRACTAL_CACHE_PATH=`pwd`/".cache"
-rm -rv ${FRACTAL_CACHE_PATH}
+rm -rv ${FRACTAL_CACHE_PATH}  2> /dev/null
 
 # Define/initialize empty project folder and temporary file
 PROJ_DIR=`pwd`/tmp_${LABEL}
-rm -r $PROJ_DIR
+rm -r $PROJ_DIR  2> /dev/null
 mkdir $PROJ_DIR
 
 ###############################################################################
 # IMPORTANT: modify the following lines so that they point to absolute paths
-INPUT_PATH=$BASE_FOLDER_EXAMPLE/images/10.5281_zenodo.7059515
+INPUT_PATH=`pwd`/../images/10.5281_zenodo.7059515
 OUTPUT_PATH=${PROJ_DIR}/output
 ###############################################################################
 
@@ -64,7 +60,6 @@ fractal workflow add-task $WF_ID "Maximum Intensity Projection"
 fractal workflow add-task $WF_ID "Cellpose Segmentation" --args-file Parameters/args_cellpose_segmentation.json --meta-file Parameters/example_meta.json
 echo "{\"level\": 0, \"ROI_table_name\": \"well_ROI_table\", \"workflow_file\": \"`pwd`/regionprops_from_existing_labels_feature.yaml\", \"input_specs\": {\"dapi_img\": {\"type\": \"image\", \"wavelength_id\": \"A01_C01\"}, \"label_img\": {\"type\": \"label\", \"label_name\": \"nuclei\"}}, \"output_specs\": {\"regionprops_DAPI\": {\"type\": \"dataframe\",\"table_name\": \"nuclei\"}}}" > Parameters/args_measurement.json
 fractal workflow add-task $WF_ID "Napari workflows wrapper" --args-file Parameters/args_measurement.json --meta-file Parameters/example_meta.json
-
 
 # Look at the current workflows
 # fractal workflow show $WF_ID
