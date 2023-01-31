758ae4bda1edfc50650fc10cc36b0d88ebaba5a7
Joel Luethi <joel.luethi@uzh.ch>
2023-01-31 16:08:52 +0000

    Add 23 well workflow

---
+++ examples/user_specific_examples/01_CardiacDifferentiation_23wells/run_example.sh
@@ -0,0 +1,82 @@
+LABEL="cardio-23"
+
+# Get the credentials: If you followed the instructions, they can be copied 
+# from the .fractal.env file in ../00_user_setup. Alternatively, you can write
+# a .fractal.env file yourself or add --user & --password entries to all fractal
+# commands below
+cp ../../00_user_setup/.fractal.env .fractal.env
+
+# Initialization for some environment variables for the worker
+# Needed on clusters where users don't have write access to the conda env
+WORKER_INIT="\
+export CELLPOSE_LOCAL_MODELS_PATH=${HOME}/.cache/CELLPOSE_LOCAL_MODELS_PATH
+export NUMBA_CACHE_DIR=${HOME}/.cache/NUMBA_CACHE_DIR
+export NAPARI_CONFIG=${HOME}/.cache/NAPARI_CACHE_DIR
+export XDG_CONFIG_HOME=${HOME}/.cache/XDG
+"
+
+# Set useful variables
+PRJ_NAME="proj-$LABEL"
+DS_IN_NAME="input-ds-$LABEL"
+DS_OUT_NAME="output-ds-$LABEL"
+WF_NAME="Workflow $LABEL"
+
+# Set cache path and remove any previous file from there
+export FRACTAL_CACHE_PATH=`pwd`/".cache"
+rm -rv ${FRACTAL_CACHE_PATH} 2> /dev/null
+
+# Define/initialize empty project folder and temporary file
+PROJ_DIR=`pwd`/tmp_${LABEL}
+rm -r $PROJ_DIR 2> /dev/null
+mkdir $PROJ_DIR
+
+###############################################################################
+# IMPORTANT: This defines the location of input & output data
+INPUT_PATH=/data/active/jluethi/20200810-CardiomyocyteDifferentiation14/Cycle1/images_renamed
+OUTPUT_PATH=/data/active/jluethi/Fractal/20230130_23well_Cardiomyocytes
+###############################################################################
+
+# Create project
+OUTPUT=`fractal --batch project new $PRJ_NAME $PROJ_DIR`
+PRJ_ID=`echo $OUTPUT | cut -d ' ' -f1`
+DS_IN_ID=`echo $OUTPUT | cut -d ' ' -f2`
+echo "PRJ_ID: $PRJ_ID"
+echo "DS_IN_ID: $DS_IN_ID"
+
+# Update dataset name/type, and add a resource
+fractal dataset edit --name "$DS_IN_NAME" -t image --read-only $PRJ_ID $DS_IN_ID
+fractal dataset add-resource -g "*.png" $PRJ_ID $DS_IN_ID $INPUT_PATH
+
+# Add output dataset, and add a resource to it
+DS_OUT_ID=`fractal --batch project add-dataset $PRJ_ID "$DS_OUT_NAME"`
+echo "DS_OUT_ID: $DS_IN_ID"
+
+fractal dataset edit -t zarr --read-write $PRJ_ID $DS_OUT_ID
+fractal dataset add-resource -g "*.zarr" $PRJ_ID $DS_OUT_ID $OUTPUT_PATH
+
+# Create workflow
+WF_ID=`fractal --batch workflow new "$WF_NAME" $PRJ_ID`
+echo "WF_ID: $WF_ID"
+
+# Add tasks to workflow
+fractal workflow add-task $WF_ID "Create OME-Zarr structure" --args-file Parameters/create_zarr_structure.json
+fractal workflow add-task $WF_ID "Convert Yokogawa to OME-Zarr"
+
+echo "{\"overwrite\": \"True\", \"dict_corr\": {\"root_path_corr\": \"`pwd`/../../illum_corr_images/\", \"A01_C01\": \"20220621_UZH_manual_illumcorr_40x_A01_C01.png\", \"A01_C02\": \"20220621_UZH_manual_illumcorr_40x_A01_C02.png\", \"A02_C03\": \"20220621_UZH_manual_illumcorr_40x_A02_C03.png\"}}" > Parameters/illumination_correction.json
+fractal workflow add-task $WF_ID "Illumination correction" --args-file Parameters/illumination_correction.json
+
+# Maximum intensity projection
+fractal workflow add-task $WF_ID "Copy OME-Zarr structure"
+fractal workflow add-task $WF_ID "Maximum Intensity Projection"
+fractal workflow add-task $WF_ID "Cellpose Segmentation" --args-file Parameters/cellpose_segmentation.json #--meta-file Parameters/example_meta.json
+
+# Run a series of napari workflows
+echo "{\"level\": 0, \"ROI_table_name\": \"FOV_ROI_table\", \"workflow_file\": \"$PROJ_DIR/../regionprops_from_existing_labels_feature.yaml\", \"input_specs\": {\"dapi_img\": {\"type\": \"image\", \"wavelength_id\": \"A01_C01\"}, \"lamin_img\": {\"type\": \"image\", \"wavelength_id\": \"A02_C03\"}, \"nanog_img\": {\"type\": \"image\", \"wavelength_id\": \"A01_C02\"}, \"label_img\": {\"type\": \"label\", \"label_name\": \"nuclei\"}}, \"output_specs\": {\"regionprops_DAPI\": {\"type\": \"dataframe\", \"table_name\": \"regionprops_DAPI\"}, \"regionprops_lamin\": {\"type\": \"dataframe\", \"table_name\": \"regionprops_lamin\"}, \"regionprops_nanog\": {\"type\": \"dataframe\", \"table_name\": \"regionprops_nanog\"}}}" > Parameters/measurement.json
+fractal workflow add-task $WF_ID "Napari workflows wrapper" --args-file Parameters/measurement.json
+
+# Look at the current workflows
+# fractal workflow show $WF_ID
+# echo
+
+# Apply workflow
+fractal workflow apply -o $DS_OUT_ID -p $PRJ_ID $WF_ID $DS_IN_ID --worker-init "$WORKER_INIT"
