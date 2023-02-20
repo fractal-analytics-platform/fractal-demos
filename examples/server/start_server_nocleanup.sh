c766247286b6e564756fb7f9a3532fe0eb221c4b
Joel Luethi <joel.luethi@uzh.ch>
2023-02-20 10:43:56 +0000

    Restart server without cleanup

---
+++ examples/server/start_server_nocleanup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Use this script to restart a server that was previously running
+PORT=8010
+
+# Set the db path
+export SQLITE_PATH=db/fractal_server.db
+fractalctl set-db
+
+# Set environment variables
+FRACTAL_TASKS_DIR=`pwd`/FRACTAL_TASKS_DIR
+echo -e "\
+DEPLOYMENT_TYPE=testing
+JWT_SECRET_KEY=secret
+SQLITE_PATH=db/fractal_server.db
+FRACTAL_TASKS_DIR=${FRACTAL_TASKS_DIR}
+FRACTAL_LOGGING_LEVEL=20
+FRACTAL_RUNNER_BACKEND=local
+FRACTAL_RUNNER_WORKING_BASE_DIR=artifacts
+FRACTAL_SLURM_CONFIG_FILE=config_uzh.json
+FRACTAL_ADMIN_DEFAULT_EMAIL=admin@fractal.xy
+FRACTAL_ADMIN_DEFAULT_PASSWORD=1234
+" > .fractal_server.env
+
+# Start the server
+fractalctl start --port $PORT
