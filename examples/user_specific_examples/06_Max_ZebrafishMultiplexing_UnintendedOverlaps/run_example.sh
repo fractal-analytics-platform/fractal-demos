6787039fc3dc04f938e5f1316413fad565780546
Joel Luethi <joel.luethi@uzh.ch>
2023-03-17 16:05:20 +0000

    Update user_specfific_example 06 for tasks 0.9.0, server 1.1.0, client 1.1.0

--- examples/user_specific_examples/06_Max_ZebrafishMultiplexing_UnintendedOverlaps/run_example.sh
+++ examples/user_specific_examples/06_Max_ZebrafishMultiplexing_UnintendedOverlaps/run_example.sh
@@ -1,6 +1,6 @@
 ###############################################################################
 # THINGS TO BE CHANGED BY THE USER
-LABEL="max-multiplexing-cpu_high"
+LABEL="max-multiplexing"
 
 # For FMI: needs to point to the liberali group folder
 
@@ -8,14 +8,21 @@ INPUT_PATH_CYCLE_0="/data/active/fractal/3D/PelkmansLab/ZebrafishMultiplexing/cy
 INPUT_PATH_CYCLE_1="/data/active/fractal/3D/PelkmansLab/ZebrafishMultiplexing/cycle1"
 
 # Modify this to place the output somewhere else
-OUTPUT_PATH="/data/active/jluethi/Fractal/20221209_"$LABEL
+OUTPUT_PATH="/data/active/jluethi/Fractal/20230317_"$LABEL
+
+# Get the credentials: If you followed the instructions, they can be copied 
+# from the .fractal.env file in ../00_user_setup. Alternatively, you can write
+# a .fractal.env file yourself or add --user & --password entries to all fractal
+# commands below
+cp ../../00_user_setup/.fractal.env .fractal.env
 
 # Change cache directory or add proxy settings if necessary
 WORKER_INIT="\
-export CELLPOSE_LOCAL_MODELS_PATH=${HOME}/.cache/CELLPOSE_LOCAL_MODELS_PATH
-export NUMBA_CACHE_DIR=${HOME}/.cache/NUMBA_CACHE_DIR
-export NAPARI_CONFIG=${HOME}/.cache/NAPARI_CACHE_DIR
-export XDG_CONFIG_HOME=${HOME}/.cache/XDG
+export CELLPOSE_LOCAL_MODELS_PATH=$BASE_CACHE_DIR/CELLPOSE_LOCAL_MODELS_PATH
+export NUMBA_CACHE_DIR=$BASE_CACHE_DIR/NUMBA_CACHE_DIR
+export NAPARI_CONFIG=$BASE_CACHE_DIR/napari_config.json
+export XDG_CONFIG_HOME=$BASE_CACHE_DIR/XDG_CONFIG
+export XDG_CACHE_HOME=$BASE_CACHE_DIR/XDG
 "
 ###############################################################################
 
@@ -45,16 +52,16 @@ echo "PRJ_ID: $PRJ_ID"
 echo "DS_IN_ID: $DS_IN_ID"
 
 # Update dataset name/type, and add a resource
-fractal dataset edit --name "$DS_IN_NAME" -t image --read-only $PRJ_ID $DS_IN_ID
-fractal dataset add-resource -g "*.tif" $PRJ_ID $DS_IN_ID $INPUT_PATH_CYCLE_0
-fractal dataset add-resource -g "*.tif" $PRJ_ID $DS_IN_ID $INPUT_PATH_CYCLE_1
+fractal dataset edit --new-name "$DS_IN_NAME" --new-type image --make-read-only $PRJ_ID $DS_IN_ID
+fractal dataset add-resource $PRJ_ID $DS_IN_ID $INPUT_PATH_CYCLE_0
+fractal dataset add-resource $PRJ_ID $DS_IN_ID $INPUT_PATH_CYCLE_1
 
 # Add output dataset, and add a resource to it
 DS_OUT_ID=`fractal --batch project add-dataset $PRJ_ID "$DS_OUT_NAME"`
 echo "DS_OUT_ID: $DS_OUT_ID"
 
-fractal dataset edit -t zarr --read-write $PRJ_ID $DS_OUT_ID
-fractal dataset add-resource -g "*.zarr" $PRJ_ID $DS_OUT_ID $OUTPUT_PATH
+fractal dataset edit --new-type zarr --remove-read-only $PRJ_ID $DS_OUT_ID
+fractal dataset add-resource $PRJ_ID $DS_OUT_ID $OUTPUT_PATH
 
 # Create workflow
 WF_ID=`fractal --batch workflow new "$WF_NAME" $PRJ_ID`
@@ -67,7 +74,7 @@ fractal workflow add-task $WF_ID "Create OME-ZARR structure (multiplexing)" --ar
 fractal workflow add-task $WF_ID "Convert Yokogawa to OME-Zarr" --meta-file Parameters/yoko_parsing.json
 fractal workflow add-task $WF_ID "Cellpose Segmentation" --args-file Parameters/cellpose_3D_cells.json
 # Manually create measurement arg file to get the correct paths
-echo "{\"level\": 0, \"ROI_table_name\": \"FOV_ROI_table\", \"workflow_file\": \"`pwd`/regionprops_from_existing_labels_feature.yaml\", \"input_specs\": {\"dapi_img\": {\"type\": \"image\", \"wavelength_id\": \"A01_C01\"}, \"label_img\": {\"type\": \"label\", \"label_name\": \"nuclei\"}}, \"output_specs\": {\"regionprops_DAPI\": {\"type\": \"dataframe\",\"table_name\": \"nuclei\"}}}" > Parameters/args_measurement.json
+echo "{\"level\": 0, \"input_ROI_table\": \"FOV_ROI_table\", \"workflow_file\": \"`pwd`/regionprops_from_existing_labels_feature.yaml\", \"input_specs\": {\"dapi_img\": {\"type\": \"image\", \"wavelength_id\": \"A01_C01\"}, \"label_img\": {\"type\": \"label\", \"label_name\": \"nuclei\"}}, \"output_specs\": {\"regionprops_DAPI\": {\"type\": \"dataframe\",\"table_name\": \"nuclei\"}}}" > Parameters/args_measurement.json
 fractal workflow add-task $WF_ID "Napari workflows wrapper" --args-file Parameters/args_measurement.json --meta-file Parameters/meta_measurement.json
 
 # 2D processing
@@ -75,9 +82,8 @@ fractal workflow add-task $WF_ID "Copy OME-Zarr structure"
 fractal workflow add-task $WF_ID "Maximum Intensity Projection" --meta-file Parameters/yoko_parsing.json
 fractal workflow add-task $WF_ID "Cellpose Segmentation" --args-file Parameters/cellpose_mip.json
 # Add napari workflow (setting parameters here because we need to set some paths in there)
-#echo "{\"level\": 0, \"ROI_table_name\": \"FOV_ROI_table\", \"workflow_file\": \"$PROJ_DIR/../regionprops_from_existing_labels_feature.yaml\", \"input_specs\": {\"dapi_img\": {\"type\": \"image\", \"channel\": \"A04_C01\"},\"label_img\": {\"type\": \"label\", \"label_name\": \"organoids\"}}, \"output_specs\": {\"regionprops_DAPI\": {\"type\": \"dataframe\",\"table_name\": \"organoids\"}}}" > Parameters/measurement.json
-echo "{\"level\": 0, \"ROI_table_name\": \"well_ROI_table\", \"workflow_file\": \"`pwd`/regionprops_from_existing_labels_feature.yaml\", \"input_specs\": {\"dapi_img\": {\"type\": \"image\", \"wavelength_id\": \"A01_C01\"}, \"label_img\": {\"type\": \"label\", \"label_name\": \"zebrafish_embryos\"}}, \"output_specs\": {\"regionprops_DAPI\": {\"type\": \"dataframe\",\"table_name\": \"zebrafish_embryos\"}}}" > Parameters/args_measurement_2D.json
-fractal workflow add-task $WF_ID "Napari workflows wrapper" --args-file Parameters/args_measurement_2D.json --meta/^-file Parameters/yoko_parsing.json
+echo "{\"level\": 0, \"input_ROI_table\": \"well_ROI_table\", \"workflow_file\": \"`pwd`/regionprops_from_existing_labels_feature.yaml\", \"input_specs\": {\"dapi_img\": {\"type\": \"image\", \"wavelength_id\": \"A01_C01\"}, \"label_img\": {\"type\": \"label\", \"label_name\": \"zebrafish_embryos\"}}, \"output_specs\": {\"regionprops_DAPI\": {\"type\": \"dataframe\",\"table_name\": \"zebrafish_embryos\"}}}" > Parameters/args_measurement_2D.json
+fractal workflow add-task $WF_ID "Napari workflows wrapper" --args-file Parameters/args_measurement_2D.json --meta-file Parameters/meta_measurement.json
 
 # Look at the current workflows
 fractal workflow show $WF_ID
