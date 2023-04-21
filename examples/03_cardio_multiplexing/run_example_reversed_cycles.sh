be2fbabc1c024b9016c2e8799119f32d5cd73cb1
Joel Luethi <joel.luethi@uzh.ch>
2023-04-21 15:02:19 +0000

    Update example 03 for fractal 1.2.0

--- examples/03_cardio_multiplexing/run_example_reversed_cycles.sh
+++ examples/03_cardio_multiplexing/run_example_reversed_cycles.sh
@@ -1,22 +1,16 @@
-LABEL="reversed_1"
+LABEL="reversed_2"
+
+###############################################################################
+# IMPORTANT: modify the following lines so that they point to the actual input paths of the data
+INPUT_PATH=/data/active/fractal/3D/PelkmansLab/CardiacMultiplexing/tiny_multiplexing
+OUTPUT_PATH=`pwd`/tmp_$LABEL
+###############################################################################
 
 # Get the credentials: If you followed the instructions, they can be copied 
 # from the .fractal.env file in ../00_user_setup. Alternatively, you can write
 # a .fractal.env file yourself or add --user & --password entries to all fractal
 # commands below
 cp ../00_user_setup/.fractal.env .fractal.env
-
-# Initialization for some environment variables for the worker
-# Needed on clusters where users don't have write access to the conda env and 
-# fractal user cache directories
-BASE_CACHE_DIR=${HOME}/.cache
-WORKER_INIT="\
-export CELLPOSE_LOCAL_MODELS_PATH=$BASE_CACHE_DIR/CELLPOSE_LOCAL_MODELS_PATH
-export NUMBA_CACHE_DIR=$BASE_CACHE_DIR/NUMBA_CACHE_DIR
-export NAPARI_CONFIG=$BASE_CACHE_DIR/napari_config.json
-export XDG_CONFIG_HOME=$BASE_CACHE_DIR/XDG_CONFIG
-export XDG_CACHE_HOME=$BASE_CACHE_DIR/XDG
-"
 
 # Set useful variables
 PRJ_NAME="proj-$LABEL"
@@ -28,20 +22,10 @@ WF_NAME="Workflow $LABEL"
 export FRACTAL_CACHE_PATH=`pwd`/".cache"
 rm -rv ${FRACTAL_CACHE_PATH} 2> /dev/null
 
-# Define/initialize empty project folder and temporary file
-PROJ_DIR=`pwd`/tmp_${LABEL}
-rm -r $PROJ_DIR 2> /dev/null
-mkdir $PROJ_DIR
-
-###############################################################################
-# IMPORTANT: modify the following lines so that they point to the actual input paths of the data
-INPUT_PATH=/data/active/fractal/3D/PelkmansLab/CardiacMultiplexing/tiny_multiplexing
-OUTPUT_PATH=${PROJ_DIR}/output
-#OUTPUT_PATH=/data/active/jluethi/Fractal/20230119_multiplexing_$LABEL
 ###############################################################################
 
 # Create project
-OUTPUT=`fractal --batch project new $PRJ_NAME $PROJ_DIR`
+OUTPUT=`fractal --batch project new $PRJ_NAME`
 PRJ_ID=`echo $OUTPUT | cut -d ' ' -f1`
 DS_IN_ID=`echo $OUTPUT | cut -d ' ' -f2`
 echo "PRJ_ID: $PRJ_ID"
@@ -65,14 +49,10 @@ WF_ID=`fractal --batch workflow new "$WF_NAME" $PRJ_ID`
 echo "WF_ID: $WF_ID"
 
 # Add tasks to workflow
-fractal workflow add-task $WF_ID "Create OME-ZARR structure (multiplexing)" --args-file Parameters/create_zarr_structure_multiplex_reversed.json
-fractal workflow add-task $WF_ID "Convert Yokogawa to OME-Zarr"
-fractal workflow add-task $WF_ID "Copy OME-Zarr structure"
-fractal workflow add-task $WF_ID "Maximum Intensity Projection"
-
-# Look at the current workflows
-# fractal workflow show $WF_ID
-# echo
+fractal --batch workflow add-task $WF_ID "Create OME-ZARR structure (multiplexing)" --args-file Parameters/create_zarr_structure_multiplex_reversed.json  --meta-file Parameters/example_meta.json
+fractal --batch workflow add-task $WF_ID "Convert Yokogawa to OME-Zarr"
+fractal --batch workflow add-task $WF_ID "Copy OME-Zarr structure"
+fractal --batch workflow add-task $WF_ID "Maximum Intensity Projection"
 
 # Apply workflow
-fractal workflow apply -o $DS_OUT_ID -p $PRJ_ID $WF_ID $DS_IN_ID --worker-init "$WORKER_INIT"
+fractal workflow apply -o $DS_OUT_ID -p $PRJ_ID $WF_ID $DS_IN_ID
