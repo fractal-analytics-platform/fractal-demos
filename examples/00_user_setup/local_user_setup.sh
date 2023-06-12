4f66c0c51ec44092d441a0a5b5c8fd1a65c7e2f9
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-06-12 08:49:13 +0200

    Add package extra

--- examples/00_user_setup/local_user_setup.sh
+++ examples/00_user_setup/local_user_setup.sh
@@ -21,4 +21,4 @@ FRACTAL_SERVER=http://localhost:$PORT
 fractal user whoami
 
 # Trigger collection of core tasks
-fractal task collect fractal-tasks-core --package-version 0.10.0a0
+fractal task collect fractal-tasks-core --package-version 0.10.0a0 --package-extras fractal-tasks
