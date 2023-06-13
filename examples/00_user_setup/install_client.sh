1152116bcd6e1c20d2960915e892b1d3cef8e096
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-06-13 12:05:41 +0200

    Update versions

--- examples/00_user_setup/install_client.sh
+++ examples/00_user_setup/install_client.sh
@@ -1,4 +1,4 @@
-VERSION="1.3.0a2"
+VERSION="1.3.0a3"
 
 ENVNAME=fractal-client-$VERSION
 conda deactivate
--- examples/server/install_script.sh
+++ examples/server/install_script.sh
@@ -1,4 +1,4 @@
-VERSION="1.3.0a4"
+VERSION="1.3.0a5"
 
 ENVNAME=fractal-server-$VERSION
 conda deactivate
