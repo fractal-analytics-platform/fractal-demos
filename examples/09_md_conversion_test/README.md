14069a64822fbc1095b6e7eb817cb42366842ba6
jluethi <joel.luethi@uzh.ch>
2023-06-15 15:36:08 +0200

    Add MD example

---
+++ examples/09_md_conversion_test/README.md
@@ -0,0 +1,19 @@
+# Setting up a fractal client & run a workflow through Fractal
+Run a Fractal workflow with Molecular Devices Image Xpress data as input.
+
+## Client setup (from `00_user_setup` folder)
+This only needs to be done once (unless the server is restarted again). Follow the instructions in the `00_user_setup` folder.
+
+## Running an example through Fractal
+This needs to be done in each example folder you're running
+1. Switch to this example folder. If you followed the instructions above, credentials should be used automatically. Alternatively, check the top of the script to set them up manually.
+2. Get the example data: Currently just available at FMI.
+3. Make sure the MD tasks are installed. Follow instructions here: https://github.com/jluethi/fractal-faim-hcs 
+4. One can then either go through the project creation, dataset creation, workflow creation & submission one by one. Or run it all at once by running: `. ./run_example.sh`
+
+This should complete fairly quickly (submitting the script to it being finished took 30s on my machine). One can check the status with `fractal job show ID` (where the ID is the job ID of the submitted workflow, 1 for the first workflow submitted. This is shown when submitting the workflow)
+
+Check the client documentation for details on using the Fractal Client: https://fractal-analytics-platform.github.io/fractal/install.html
+Check the Fractal Tasks Core documentation for details on the individual tasks of this example workflow: https://fractal-analytics-platform.github.io/fractal-tasks-core/
+
+Successfully run with `fractal-server==1.3.0a5`, `fractal-client==1.3.0a3` and `fractal-tasks-core==0.10.0a3`
---
+++ examples/09_md_conversion_test/run_example.sh
@@ -0,0 +1,61 @@
+LABEL="MD_Parsing_2D_Test4"
+
+###############################################################################
+# IMPORTANT: This defines the location of input & output data
+INPUT_PATH="/Users/joel/Desktop/230219MK004EB-R1Bleach"
+OUTPUT_PATH="/Users/joel/Desktop/MD_Test_$LABEL"
+###############################################################################
+
+# Get the credentials: If you followed the instructions, they can be copied 
+# from the .fractal.env file in ../00_user_setup. Alternatively, you can write
+# a .fractal.env file yourself or add --user & --password entries to all fractal
+# commands below
+cp ../00_user_setup/.fractal.env .fractal.env
+
+# Set useful variables
+PRJ_NAME="proj-$LABEL"
+DS_IN_NAME="input-ds-$LABEL"
+DS_OUT_NAME="output-ds-$LABEL"
+WF_NAME="Workflow $LABEL"
+
+# Set cache path and remove any previous file from there
+export FRACTAL_CACHE_PATH=`pwd`/".cache"
+rm -rv ${FRACTAL_CACHE_PATH}  2> /dev/null
+
+###############################################################################
+
+# Create project
+OUTPUT=`fractal --batch project new $PRJ_NAME`
+PRJ_ID=`echo $OUTPUT | cut -d ' ' -f1`
+DS_IN_ID=`echo $OUTPUT | cut -d ' ' -f2`
+echo "PRJ_ID: $PRJ_ID"
+echo "DS_IN_ID: $DS_IN_ID"
+
+# Update dataset name/type, and add a resource
+fractal dataset edit --new-name "$DS_IN_NAME" --new-type image --make-read-only $PRJ_ID $DS_IN_ID
+fractal dataset add-resource $PRJ_ID $DS_IN_ID $INPUT_PATH
+
+# Add output dataset, and add a resource to it
+DS_OUT_ID=`fractal --batch project add-dataset $PRJ_ID "$DS_OUT_NAME"`
+echo "DS_OUT_ID: $DS_OUT_ID"
+
+fractal dataset edit --new-type zarr --remove-read-only $PRJ_ID $DS_OUT_ID
+fractal dataset add-resource $PRJ_ID $DS_OUT_ID $OUTPUT_PATH
+
+# Create workflow
+WF_ID=`fractal --batch workflow new "$WF_NAME" $PRJ_ID`
+echo "WF_ID: $WF_ID"
+
+###############################################################################
+
+# Add tasks to workflow
+fractal --batch workflow add-task $PRJ_ID $WF_ID --task-name "Create OME-Zarr MD" --args-file Parameters/args_create_ome_zarr.json
+fractal --batch workflow add-task $PRJ_ID $WF_ID --task-name "Convert MD to OME-Zarr"
+# fractal --batch workflow add-task $PRJ_ID $WF_ID --task-name "Copy OME-Zarr structure"
+# fractal --batch workflow add-task $PRJ_ID $WF_ID --task-name "Maximum Intensity Projection"
+fractal --batch workflow add-task $PRJ_ID $WF_ID --task-name "Cellpose Segmentation" --args-file Parameters/args_cellpose_segmentation.json
+echo "{\"level\": 0, \"expected_dimensions\": 2, \"input_ROI_table\": \"well_ROI_table\", \"workflow_file\": \"`pwd`/regionprops_from_existing_labels_feature.yaml\", \"input_specs\": {\"dapi_img\": {\"type\": \"image\", \"wavelength_id\": \"C01\"}, \"label_img\": {\"type\": \"label\", \"label_name\": \"organoids\"}}, \"output_specs\": {\"regionprops_DAPI\": {\"type\": \"dataframe\",\"table_name\": \"nuclei\"}}}" > Parameters/args_measurement.json
+fractal --batch workflow add-task $PRJ_ID $WF_ID --task-name "Napari workflows wrapper" --args-file Parameters/args_measurement.json --meta-file Parameters/example_meta.json
+
+# Apply workflow
+fractal workflow apply $PRJ_ID $WF_ID $DS_IN_ID $DS_OUT_ID
