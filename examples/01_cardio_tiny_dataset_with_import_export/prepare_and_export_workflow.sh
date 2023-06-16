2ea25e161a0da02aafe59ca949f3efdbc94b153a
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-06-16 08:56:23 +0200

    Update 01_cardio_tiny_dataset_with_import_export

--- examples/01_cardio_tiny_dataset_with_import_export/prepare_and_export_workflow.sh
+++ examples/01_cardio_tiny_dataset_with_import_export/prepare_and_export_workflow.sh
@@ -6,7 +6,7 @@ cp ../00_user_setup/.fractal.env .fractal.env
 
 # Create dummy project (note: this is only because a new workflow must be
 # associated to a project)
-OUTPUT=`fractal --batch project new dummy-project-2`
+OUTPUT=`fractal --batch project new dummy-project-5`
 PRJ_ID=`echo $OUTPUT | cut -d ' ' -f1`
 echo "PRJ_ID: $PRJ_ID"
 
@@ -16,12 +16,11 @@ WF_ID=`fractal --batch workflow new "$WF_NAME" $PRJ_ID`
 echo "WF_ID: $WF_ID"
 
 # Add tasks to workflow
-fractal --batch workflow add-task $WF_ID "Create OME-Zarr structure" --args-file Parameters/args_create_ome_zarr.json --meta-file Parameters/example_meta.json
-fractal --batch workflow add-task $WF_ID "Convert Yokogawa to OME-Zarr"
-fractal --batch workflow add-task $WF_ID "Copy OME-Zarr structure"
-fractal --batch workflow add-task $WF_ID "Maximum Intensity Projection"
-fractal --batch workflow add-task $WF_ID "Cellpose Segmentation" --args-file Parameters/args_cellpose_segmentation.json --meta-file Parameters/example_meta.json
-echo "{\"level\": 0, \"input_ROI_table\": \"well_ROI_table\", \"workflow_file\": \"`pwd`/regionprops_from_existing_labels_feature.yaml\", \"input_specs\": {\"dapi_img\": {\"type\": \"image\", \"wavelength_id\": \"A01_C01\"}, \"label_img\": {\"type\": \"label\", \"label_name\": \"nuclei\"}}, \"output_specs\": {\"regionprops_DAPI\": {\"type\": \"dataframe\",\"table_name\": \"nuclei\"}}}" > Parameters/args_measurement.json
-fractal --batch workflow add-task $WF_ID "Napari workflows wrapper" --args-file Parameters/args_measurement.json --meta-file Parameters/example_meta.json
+fractal --batch workflow add-task $PRJ_ID $WF_ID --task-name "Create OME-Zarr structure" --args-file Parameters/args_create_ome_zarr.json --meta-file Parameters/example_meta.json
+fractal --batch workflow add-task $PRJ_ID $WF_ID --task-name "Convert Yokogawa to OME-Zarr"
+fractal --batch workflow add-task $PRJ_ID $WF_ID --task-name "Copy OME-Zarr structure"
+fractal --batch workflow add-task $PRJ_ID $WF_ID --task-name "Maximum Intensity Projection"
+fractal --batch workflow add-task $PRJ_ID $WF_ID --task-name "Cellpose Segmentation" --args-file Parameters/args_cellpose_segmentation.json --meta-file Parameters/example_meta.json
+fractal --batch workflow add-task $PRJ_ID $WF_ID --task-name "Napari workflows wrapper" --args-file Parameters/args_measurement.json --meta-file Parameters/example_meta.json
 
-fractal --batch workflow export $WF_ID --json-file workflow.json
+fractal --batch workflow export $PRJ_ID $WF_ID --json-file workflow.json
