c0350d7ef9dc3bb8d358ebd9720385e77bf88365
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-06-19 17:07:05 +0200

    Keep updating scripts in example 04

--- examples/04_cardio_illumination_correction/run_example_with_illumCorr.sh
+++ examples/04_cardio_illumination_correction/run_example_with_illumCorr.sh
@@ -47,19 +47,19 @@ WF_ID=`fractal --batch workflow new "$WF_NAME" $PRJ_ID`
 echo "WF_ID: $WF_ID"
 
 # Add tasks to workflow
-fractal --batch workflow add-task $WF_ID "Create OME-Zarr structure" --args-file Parameters/args_create_ome_zarr_illum_corr.json --meta-file Parameters/example_meta.json
-fractal --batch workflow add-task $WF_ID "Convert Yokogawa to OME-Zarr"
+fractal --batch workflow add-task $PRJ_ID $WF_ID --task-name "Create OME-Zarr structure" --args-file Parameters/args_create_ome_zarr_illum_corr.json --meta-file Parameters/example_meta.json
+fractal --batch workflow add-task $PRJ_ID $WF_ID --task-name "Convert Yokogawa to OME-Zarr"
 echo "{\"overwrite\": \"True\", \"dict_corr\": {\"root_path_corr\": \"`pwd`/../illum_corr_images/\", \"A01_C01\": \"20220621_UZH_manual_illumcorr_40x_A01_C01.png\", \"A01_C02\": \"20220621_UZH_manual_illumcorr_40x_A01_C02.png\", \"A02_C03\": \"20220621_UZH_manual_illumcorr_40x_A02_C03.png\"}}" > Parameters/illumination_correction.json
-fractal --batch workflow add-task $WF_ID "Illumination correction" --args-file Parameters/illumination_correction.json
-fractal --batch workflow add-task $WF_ID "Copy OME-Zarr structure"
-fractal --batch workflow add-task $WF_ID "Maximum Intensity Projection"
-fractal --batch workflow add-task $WF_ID "Cellpose Segmentation" --args-file Parameters/args_cellpose_segmentation.json --meta-file Parameters/cellpose_meta.json
+fractal --batch workflow add-task $PRJ_ID $WF_ID --task-name "Illumination correction" --args-file Parameters/illumination_correction.json
+fractal --batch workflow add-task $PRJ_ID $WF_ID --task-name "Copy OME-Zarr structure"
+fractal --batch workflow add-task $PRJ_ID $WF_ID --task-name "Maximum Intensity Projection"
+fractal --batch workflow add-task $PRJ_ID $WF_ID --task-name "Cellpose Segmentation" --args-file Parameters/args_cellpose_segmentation.json --meta-file Parameters/cellpose_meta.json
 echo "{\"level\": 0, \"input_ROI_table\": \"well_ROI_table\", \"workflow_file\": \"`pwd`/regionprops_with_coordinates.yaml\", \"input_specs\": {\"dapi_img\": {\"type\": \"image\", \"wavelength_id\": \"A01_C01\"}, \"label_img\": {\"type\": \"label\", \"label_name\": \"nuclei\"}}, \"output_specs\": {\"regionprops_DAPI\": {\"type\": \"dataframe\",\"table_name\": \"nuclei_C01\"}}}" > Parameters/args_measurement_C01.json
-fractal --batch workflow add-task $WF_ID "Napari workflows wrapper" --args-file Parameters/args_measurement_C01.json
+fractal --batch workflow add-task $PRJ_ID $WF_ID --task-name "Napari workflows wrapper" --args-file Parameters/args_measurement_C01.json
 echo "{\"level\": 0, \"input_ROI_table\": \"well_ROI_table\", \"workflow_file\": \"`pwd`/regionprops_with_coordinates.yaml\", \"input_specs\": {\"dapi_img\": {\"type\": \"image\", \"wavelength_id\": \"A01_C02\"}, \"label_img\": {\"type\": \"label\", \"label_name\": \"nuclei\"}}, \"output_specs\": {\"regionprops_DAPI\": {\"type\": \"dataframe\",\"table_name\": \"nuclei_C02\"}}}" > Parameters/args_measurement_C02.json
-fractal --batch workflow add-task $WF_ID "Napari workflows wrapper" --args-file Parameters/args_measurement_C02.json
+fractal --batch workflow add-task $PRJ_ID $WF_ID --task-name "Napari workflows wrapper" --args-file Parameters/args_measurement_C02.json
 echo "{\"level\": 0, \"input_ROI_table\": \"well_ROI_table\", \"workflow_file\": \"`pwd`/regionprops_with_coordinates.yaml\", \"input_specs\": {\"dapi_img\": {\"type\": \"image\", \"wavelength_id\": \"A02_C03\"}, \"label_img\": {\"type\": \"label\", \"label_name\": \"nuclei\"}}, \"output_specs\": {\"regionprops_DAPI\": {\"type\": \"dataframe\",\"table_name\": \"nuclei_C03\"}}}" > Parameters/args_measurement_C03.json
-fractal --batch workflow add-task $WF_ID "Napari workflows wrapper" --args-file Parameters/args_measurement_C03.json
+fractal --batch workflow add-task $PRJ_ID $WF_ID --task-name "Napari workflows wrapper" --args-file Parameters/args_measurement_C03.json
 
 # Apply workflow
 fractal workflow apply -o $DS_OUT_ID -p $PRJ_ID $WF_ID $DS_IN_ID
