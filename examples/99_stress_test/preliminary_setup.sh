b9eb8a8d0c13a11991f9b8fb9f9c7b4ecf63c2a6
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-06-28 12:21:24 +0000

    Update script in 99_stress_test

--- examples/99_stress_test/preliminary_setup.sh
+++ examples/99_stress_test/preliminary_setup.sh
@@ -1,5 +1,8 @@
 cp ../00_user_setup/.fractal.env .
+cp ../00_user_setup/.fractal.env A_many_running_workflows_and_tasks/
+cp ../00_user_setup/.fractal.env B_test_db_connections/
+
 
 VERSION="2.0.0"
 fractal task collect `pwd`/fractal-tasks-stresstest/dist/fractal_tasks_stresstest-${VERSION}-py3-none-any.whl
-echo "Check status with: fractal task check-collection 1"
+echo "Check status with: fractal task check-collection ID"
