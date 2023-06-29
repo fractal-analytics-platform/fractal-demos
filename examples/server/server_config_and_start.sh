ac689d5ba76253c050c99f3900f5ee03b4f4a701
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-06-29 14:08:54 +0200

    Update `examples/server` startup scripts
    1. Make them homegeneous (same `local` backend, same ports)
    2. Include `--host` option
    3. Use SQLITE_PATH variable in `.fractal_server.env` file, since it is already defined
    4. Clarify comments

--- examples/server/server_config_and_start.sh
+++ examples/server/server_config_and_start.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Use this script to startup a clean fractal-server instance (with an empty
+# database, and without folders for artifacts and task venvs).
+
+HOST=localhost
 PORT=8010
 
 # Create an empty db
@@ -17,10 +21,10 @@ FRACTAL_TASKS_DIR=`pwd`/FRACTAL_TASKS_DIR
 echo -e "\
 DEPLOYMENT_TYPE=testing
 JWT_SECRET_KEY=secret
-SQLITE_PATH=db/fractal_server.db
+SQLITE_PATH=${SQLITE_PATH}
 FRACTAL_TASKS_DIR=${FRACTAL_TASKS_DIR}
 FRACTAL_LOGGING_LEVEL=20
-FRACTAL_RUNNER_BACKEND=slurm
+FRACTAL_RUNNER_BACKEND=local
 FRACTAL_RUNNER_WORKING_BASE_DIR=artifacts
 FRACTAL_SLURM_CONFIG_FILE=config_uzh.json
 FRACTAL_ADMIN_DEFAULT_EMAIL=admin@fractal.xy
@@ -28,6 +32,5 @@ FRACTAL_ADMIN_DEFAULT_PASSWORD=1234
 JWT_EXPIRE_SECONDS=84600
 " > .fractal_server.env
 
-
 # Start the server
-fractalctl start --port $PORT
+fractalctl start --host $HOST --port $PORT
