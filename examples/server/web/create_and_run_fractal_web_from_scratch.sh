4bdb817f92b1104c8f13859f170b914805024523
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-06-29 14:36:05 +0200

    Fix self-contained script to fetch&run fractal-web

--- examples/server/web/create_and_run_fractal_web_from_scratch.sh
+++ examples/server/web/create_and_run_fractal_web_from_scratch.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+PORT_WEB=5173
+PORT_API=8010
 RELEASE="0.5.1"
+
 echo "*** Fetch a clean copy of fractal-web ${RELEASE} **"
 rm -fr fractal-web-${RELEASE} 
 rm -f ${RELEASE}.tar.gz
@@ -19,7 +22,7 @@ npm install
 # Write down environment variables
 echo "*** Write environment variables for development mode ***"
 echo "
-FRACTAL_SERVER_HOST=http://localhost:8010
+FRACTAL_SERVER_HOST=http://localhost:$PORT_API
 AUTH_COOKIE_NAME=fastapiusersauth
 AUTH_COOKIE_SECURE=false
 AUTH_COOKIE_DOMAIN=localhost
@@ -28,5 +31,7 @@ AUTH_COOKIE_MAX_AGE=1800
 AUTH_COOKIE_SAME_SITE=lax
 AUTH_COOKIE_HTTP_ONLY=true
 " > .env #.development
+
 npm run build
-PORT=5173 node build/
+
+ORIGIN=http://localhost:$PORT_WEB PORT=$PORT_WEB node build
