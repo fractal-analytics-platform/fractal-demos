509c91ef23818b3a8a8040c39f0aca41f9dde118
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-06-29 14:06:14 +0200

    Update create_and_run_fractal_web_from_scratch.sh

--- examples/server/web/create_and_run_fractal_web_from_scratch.sh
+++ examples/server/web/create_and_run_fractal_web_from_scratch.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-# Fetch a clean fractal-web copy
-rm -fr fractal-web
-git clone git@github.com:fractal-analytics-platform/fractal-web.git
-cd fractal-web
-git checkout 0.5.1
+RELEASE="0.5.1"
+echo "*** Fetch a clean copy of fractal-web ${RELEASE} **"
+rm -fr fractal-web-${RELEASE} 
+rm -f ${RELEASE}.tar.gz
+wget https://github.com/fractal-analytics-platform/fractal-web/archive/refs/tags/${RELEASE}.tar.gz
+tar -xvf ${RELEASE}.tar.gz
+rm ${RELEASE}.tar.gz
+
+# Enter folder and install dependencies
+echo "*** Change folder ***"
+cd fractal-web-${RELEASE}
+pwd
+
+echo "*** Install dependencies ***"
 npm install
 
 # Write down environment variables
+echo "*** Write environment variables for development mode ***"
 echo "
 FRACTAL_SERVER_HOST=http://localhost:8010
 AUTH_COOKIE_NAME=fastapiusersauth
@@ -17,6 +27,6 @@ AUTH_COOKIE_PATH=/
 AUTH_COOKIE_MAX_AGE=1800
 AUTH_COOKIE_SAME_SITE=lax
 AUTH_COOKIE_HTTP_ONLY=true
-" > .env.development
-
-npm run dev
+" > .env #.development
+npm run build
+PORT=5173 node build/
