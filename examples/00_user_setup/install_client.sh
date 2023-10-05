ef02889202c5435f91b6b29166536ae2e5a0edce
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-10-05 09:21:06 +0200

    Install server&client from conda-forge

--- examples/00_user_setup/install_client.sh
+++ examples/00_user_setup/install_client.sh
@@ -5,6 +5,5 @@ conda deactivate
 conda remove --name $ENVNAME -y --all
 conda create --name $ENVNAME python==3.10 -y
 conda activate $ENVNAME
-conda update pip -y
 
-pip install fractal-client==$VERSION
+conda install fractal-client==$VERSION
--- examples/server/install_script.sh
+++ examples/server/install_script.sh
@@ -5,7 +5,5 @@ conda deactivate
 conda remove --name $ENVNAME -y --all
 conda create --name $ENVNAME python==3.10 -y
 conda activate $ENVNAME
-conda update pip -y
 
-pip install "fractal-server[slurm,gunicorn,postgres]"==$VERSION
-# pip install fractal-server==$VERSION
+conda install "fractal-server[slurm,gunicorn,postgres]"==$VERSION
