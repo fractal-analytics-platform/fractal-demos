ba6399f8b9a549955cc8e235143969ac0be79d3c
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2023-10-10 15:56:10 +0200

    fix 10 and 99

--- examples/99_stress_test/B_test_db_connections/run_example.sh
+++ examples/99_stress_test/B_test_db_connections/run_example.sh
@@ -4,24 +4,23 @@
 LABEL=$1
 
 # Create project and I/O datasets
-OUTPUT=`fractal --batch project new "project-$LABEL"`
-PRJ_ID=`echo $OUTPUT | cut -d ' ' -f1`
-DS_IN_ID=`echo $OUTPUT | cut -d ' ' -f2`
-fractal --batch dataset add-resource $PRJ_ID $DS_IN_ID `pwd`/tmp
-DS_OUT_ID=`fractal --batch project add-dataset $PRJ_ID "output-ds-$LABEL"`
+PROJECT_ID=`fractal --batch project new "project-$LABEL"`
+DS_IN_ID=`fractal --batch project add-dataset $PROJECT_ID "$DS_IN_NAME" --type image --make-read-only`
+fractal --batch dataset add-resource $PROJECT_ID $DS_IN_ID `pwd`/tmp
+DS_OUT_ID=`fractal --batch project add-dataset $PROJECT_ID "output-ds-$LABEL"`
 OUTPUT_PATH=`pwd`/output-$LABEL
 mkdir -p $OUTPUT_PATH
-fractal --batch dataset add-resource $PRJ_ID $DS_OUT_ID $OUTPUT_PATH
+fractal --batch dataset add-resource $PROJECT_ID $DS_OUT_ID $OUTPUT_PATH
 
 # TEST 1: create dummy workflow and add the same task N times
-WF_ID=`fractal --batch workflow new "dummy-workflow-$LABEL" $PRJ_ID`
+WF_ID=`fractal --batch workflow new "dummy-workflow-$LABEL" $PROJECT_ID`
 for IND_WFTASK in {1..20}; do
     echo "Add WorkflowTask $IND_WFTASK/20"
-    fractal --batch workflow add-task $PRJ_ID $WF_ID --task-name "prepare_metadata"
+    fractal --batch workflow add-task $PROJECT_ID $WF_ID --task-name "prepare_metadata"
 done
 
 # Create actual workflow
-WF_ID=`fractal --batch workflow new "workflow-$LABEL" $PRJ_ID`
+WF_ID=`fractal --batch workflow new "workflow-$LABEL" $PROJECT_ID`
 mkdir -p Parameters
 
 # Create parameter files
@@ -29,12 +28,12 @@ echo "{\"num_components\": 2}" > Parameters/prepare_metadata-args.json
 echo "{\"sleep_time\": 30}" > Parameters/sleep_task-args.json
 
 # Add tasks to workflow
-fractal --batch workflow add-task $PRJ_ID $WF_ID --task-name "prepare_metadata" --args-file Parameters/prepare_metadata-args.json
-fractal --batch workflow add-task $PRJ_ID $WF_ID --task-name "sleep_task" --args-file Parameters/sleep_task-args.json
+fractal --batch workflow add-task $PROJECT_ID $WF_ID --task-name "prepare_metadata" --args-file Parameters/prepare_metadata-args.json
+fractal --batch workflow add-task $PROJECT_ID $WF_ID --task-name "sleep_task" --args-file Parameters/sleep_task-args.json
 
 # TEST 2: Apply workflow N times
 for IND_APPLY in {1..20}; do
     echo "Call workflow-apply $IND_APPLY/20"
-    fractal --batch workflow apply $PRJ_ID $WF_ID $DS_IN_ID $DS_OUT_ID
+    fractal --batch workflow apply $PROJECT_ID $WF_ID $DS_IN_ID $DS_OUT_ID
     echo
 done
--- examples/99_stress_test/C_long_runnning_workflow/run_example.sh
+++ examples/99_stress_test/C_long_runnning_workflow/run_example.sh
@@ -5,7 +5,7 @@ LABEL=longrun-$1
 ###############################################################################
 
 # Set useful variables
-PRJ_NAME="proj-$LABEL"
+PRJPROJECT_NAME="proj-$LABEL"
 DS_OUT_NAME="output-ds-$LABEL"
 WF_NAME="Workflow $LABEL"
 
@@ -17,21 +17,20 @@ mkdir -p $OUTPUT_PATH
 ###############################################################################
 
 # Create project
-OUTPUT=`fractal --batch project new $PRJ_NAME`
-PRJ_ID=`echo $OUTPUT | cut -d ' ' -f1`
-DS_IN_ID=`echo $OUTPUT | cut -d ' ' -f2`
+PROJECT_ID=`fractal --batch project new $PROJECT_NAME`
 
-# Update dataset name/type, and add a resource
-fractal --batch dataset add-resource $PRJ_ID $DS_IN_ID $INPUT_PATH
+# Add input dataset, and add a resource to it
+DS_IN_ID=`fractal --batch project add-dataset $PROJECT_ID "$DS_IN_NAME" --type image --make-read-only`
+fractal --batch dataset add-resource $PROJECT_ID $DS_IN_ID $INPUT_PATH
 
 # Add output dataset, and add a resource to it
-DS_OUT_ID=`fractal --batch project add-dataset $PRJ_ID "$DS_OUT_NAME"`
-fractal --batch dataset add-resource $PRJ_ID $DS_OUT_ID $OUTPUT_PATH
+DS_OUT_ID=`fractal --batch project add-dataset $PROJECT_ID "$DS_OUT_NAME"`
+fractal --batch dataset add-resource $PROJECT_ID $DS_OUT_ID $OUTPUT_PATH
 
 # Create workflow and add tasks
-WF_ID=`fractal --batch workflow new "$WF_NAME" $PRJ_ID`
-fractal --batch workflow add-task $PRJ_ID $WF_ID --task-name "prepare_metadata" --args-file Parameters/prepare_metadata-args.json
-fractal --batch workflow add-task $PRJ_ID $WF_ID --task-name "sleep_task" --args-file Parameters/sleep_task-args.json
+WF_ID=`fractal --batch workflow new "$WF_NAME" $PROJECT_ID`
+fractal --batch workflow add-task $PROJECT_ID $WF_ID --task-name "prepare_metadata" --args-file Parameters/prepare_metadata-args.json
+fractal --batch workflow add-task $PROJECT_ID $WF_ID --task-name "sleep_task" --args-file Parameters/sleep_task-args.json
 
 # Apply workflow
-fractal --batch workflow apply $PRJ_ID $WF_ID $DS_IN_ID $DS_OUT_ID
+fractal --batch workflow apply $PROJECT_ID $WF_ID $DS_IN_ID $DS_OUT_ID
