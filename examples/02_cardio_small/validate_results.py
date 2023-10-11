776ca4aa435cba14c15658f269fe7435d7e39c4b
Yuri Chiucconi <yuri.chiucconi@exact-lab.it>
2023-10-11 10:44:06 +0200

    cp validate_results.py

---
+++ examples/02_cardio_small/validate_results.py
@@ -0,0 +1,46 @@
+import zarr
+import itertools
+import logging
+import sys
+from pathlib import Path
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s; %(levelname)s; %(message)s"
+)
+
+outdir = Path("output_cardiac-tiny")
+f1 = outdir / "20200812-CardiomyocyteDifferentiation14-Cycle1.zarr"
+f2 = outdir / "20200812-CardiomyocyteDifferentiation14-Cycle1_mip.zarr"
+
+
+for path in [
+        f1,
+        f1 / "B/03/0",
+        f1 / "B/03/0/tables",
+        f1 / "B/03/0/tables/well_ROI_table",
+        f1 / "B/03/0/tables/FOV_ROI_table",
+        f2,
+        f2 / "B/03/0",
+        f2 / "B/03/0/tables",
+        f2 / "B/03/0/tables/well_ROI_table",
+        f2 / "B/03/0/tables/FOV_ROI_table",
+        ]:
+    try:
+        g = zarr.open_group(path.as_posix(), "r")
+        logging.info(f"{path} is a valid zarr group")
+        logging.info(f"Sub-groups/arrays: {list(g.keys())}")
+    except Exception as e:
+        logging.error(f"{path} is not a valid zarr group")
+        logging.error(f"Original error: {str(e)}")
+        sys.exit(1)
+
+for (f, level) in itertools.product((f1, f2), range(5)):
+    try:
+        path = f / "B/03/0" / str(level)
+        a = zarr.open_array(path.as_posix(), "r")
+        logging.info(f"{path} is a valid zarr array")
+    except Exception as e:
+        logging.error(f"{path} is not a valid zarr array")
+        logging.error(f"Original error: {str(e)}")
+        sys.exit(1)
