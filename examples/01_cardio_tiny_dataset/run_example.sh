065c32f6051873b7b06996c7df5959c0b135bd23
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2023-10-13 11:06:11 +0200

    Revert WORKFLOW to WF

--- examples/01_cardio_tiny_dataset/run_example.sh
+++ examples/01_cardio_tiny_dataset/run_example.sh
@@ -16,7 +16,7 @@ cp ../00_user_setup/.fractal.env .fractal.env
 PROJECT_NAME="proj-$LABEL"
 DS_IN_NAME="input-ds-$LABEL"
 DS_OUT_NAME="output-ds-$LABEL"
-WORKFLOW_NAME="Workflow $LABEL"
+WF_NAME="Workflow $LABEL"
 
 # Set cache path and remove any previous file from there
 export FRACTAL_CACHE_PATH=`pwd`/".cache"
@@ -39,8 +39,8 @@ echo "DS_OUT_ID=$DS_OUT_ID"
 fractal dataset add-resource $PROJECT_ID $DS_OUT_ID $OUTPUT_PATH
 
 # Create workflow
-WORKFLOW_ID=`fractal --batch workflow new "$WORKFLOW_NAME" $PROJECT_ID`
-echo "WORKFLOW_ID=$WORKFLOW_ID"
+WF_ID=`fractal --batch workflow new "$WF_NAME" $PROJECT_ID`
+echo "WF_ID=$WF_ID"
 
 ###############################################################################
 
@@ -64,13 +64,13 @@ echo "{
 ###############################################################################
 
 # Add tasks to workflow
-fractal --batch workflow add-task $PROJECT_ID $WORKFLOW_ID --task-name "Create OME-Zarr structure" --args-file Parameters/args_create_ome_zarr.json --meta-file Parameters/example_meta.json
-fractal --batch workflow add-task $PROJECT_ID $WORKFLOW_ID --task-name "Convert Yokogawa to OME-Zarr"
-fractal --batch workflow add-task $PROJECT_ID $WORKFLOW_ID --task-name "Copy OME-Zarr structure" --args-file Parameters/copy_ome_zarr.json
-fractal --batch workflow add-task $PROJECT_ID $WORKFLOW_ID --task-name "Maximum Intensity Projection"
-fractal --batch workflow add-task $PROJECT_ID $WORKFLOW_ID --task-name "Cellpose Segmentation" --args-file Parameters/args_cellpose_segmentation.json #--meta-file Parameters/cellpose_meta.json
-fractal --batch workflow add-task $PROJECT_ID $WORKFLOW_ID --task-name "Napari workflows wrapper" --args-file Parameters/args_measurement.json --meta-file Parameters/example_meta.json
+fractal --batch workflow add-task $PROJECT_ID $WF_ID --task-name "Create OME-Zarr structure" --args-file Parameters/args_create_ome_zarr.json --meta-file Parameters/example_meta.json
+fractal --batch workflow add-task $PROJECT_ID $WF_ID --task-name "Convert Yokogawa to OME-Zarr"
+fractal --batch workflow add-task $PROJECT_ID $WF_ID --task-name "Copy OME-Zarr structure" --args-file Parameters/copy_ome_zarr.json
+fractal --batch workflow add-task $PROJECT_ID $WF_ID --task-name "Maximum Intensity Projection"
+fractal --batch workflow add-task $PROJECT_ID $WF_ID --task-name "Cellpose Segmentation" --args-file Parameters/args_cellpose_segmentation.json #--meta-file Parameters/cellpose_meta.json
+fractal --batch workflow add-task $PROJECT_ID $WF_ID --task-name "Napari workflows wrapper" --args-file Parameters/args_measurement.json --meta-file Parameters/example_meta.json
 
 # Apply workflow
-JOB_ID=`fractal --batch workflow apply $PROJECT_ID $WORKFLOW_ID $DS_IN_ID $DS_OUT_ID`
+JOB_ID=`fractal --batch workflow apply $PROJECT_ID $WF_ID $DS_IN_ID $DS_OUT_ID`
 echo "JOB_ID=$JOB_ID"  # Do not remove this line, it's used in fractal-containers
