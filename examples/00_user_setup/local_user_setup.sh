017b379824d6c85b534c09cc4c794c306b2083c9
jluethi <joel.luethi@uzh.ch>
2024-02-07 17:35:30 +0100

    Minor updates to local user setup

--- examples/00_user_setup/local_user_setup.sh
+++ examples/00_user_setup/local_user_setup.sh
@@ -8,7 +8,6 @@ DEFAULT_ADMIN="admin@fractal.xy"
 DEFAULT_ADMIN_PW="1234"
 NEW_USER_EMAIL=user@fractal.xy
 NEW_USER_PWD="my_pwd"
-# ADMIN_ID=`fractal --user $DEFAULT_ADMIN --password $DEFAULT_ADMIN_PW --batch user whoami`
 fractal --user $DEFAULT_ADMIN --password $DEFAULT_ADMIN_PW user register $NEW_USER_EMAIL $NEW_USER_PWD
 
 # Write credentials in a .env file (optional) and check new identity
@@ -20,4 +19,4 @@ FRACTAL_SERVER=http://localhost:$PORT
 fractal user whoami
 
 # Trigger collection of core tasks
-fractal task collect fractal-tasks-core --package-version 0.14.0a0 --package-extras fractal-tasks
+fractal task collect fractal-tasks-core --package-version 0.14.2 --package-extras fractal-tasks
