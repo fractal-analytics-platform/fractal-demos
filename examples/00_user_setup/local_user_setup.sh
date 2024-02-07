d18f058aef63fc9097c0e6a9662980ab55eef431
jluethi <joel.luethi@uzh.ch>
2024-02-07 17:34:38 +0100

    Update local user setup: Create new user instead of renaming admin

--- examples/00_user_setup/local_user_setup.sh
+++ examples/00_user_setup/local_user_setup.sh
@@ -6,15 +6,15 @@ PORT=8010
 # Modify default admin credentials
 DEFAULT_ADMIN="admin@fractal.xy"
 DEFAULT_ADMIN_PW="1234"
-NEW_ADMIN_EMAIL=real-admin@fractal.xy
-NEW_ADMIN_PWD="new_pwd"
-ADMIN_ID=`fractal --user $DEFAULT_ADMIN --password $DEFAULT_ADMIN_PW --batch user whoami`
-fractal --user $DEFAULT_ADMIN --password $DEFAULT_ADMIN_PW user edit $ADMIN_ID --new-email $NEW_ADMIN_EMAIL --new-password $NEW_ADMIN_PWD
+NEW_USER_EMAIL=user@fractal.xy
+NEW_USER_PWD="my_pwd"
+# ADMIN_ID=`fractal --user $DEFAULT_ADMIN --password $DEFAULT_ADMIN_PW --batch user whoami`
+fractal --user $DEFAULT_ADMIN --password $DEFAULT_ADMIN_PW user register $NEW_USER_EMAIL $NEW_USER_PWD
 
 # Write credentials in a .env file (optional) and check new identity
 echo -e "\
-FRACTAL_USER=$NEW_ADMIN_EMAIL
-FRACTAL_PASSWORD=$NEW_ADMIN_PWD
+FRACTAL_USER=$NEW_USER_EMAIL
+FRACTAL_PASSWORD=$NEW_USER_PWD
 FRACTAL_SERVER=http://localhost:$PORT
 " > .fractal.env
 fractal user whoami
