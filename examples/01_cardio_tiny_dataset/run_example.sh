4b218236b18a9d175052989ffd6e3e26a6bf5635
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2024-04-18 10:56:02 +0200

    Add `set -e` to example 01

--- examples/01_cardio_tiny_dataset/run_example.sh
+++ examples/01_cardio_tiny_dataset/run_example.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
 LABEL="cardiac-tiny"
 
 ###############################################################################
