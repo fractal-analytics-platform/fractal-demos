585c968e346a8602009b8a5cc537e57f4bc23072
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2024-05-20 09:54:35 +0200

    Stash

---
+++ examples/01_cardio_tiny_dataset/submissions.sh
@@ -0,0 +1,11 @@
+
+fractal --batch job submit 155 155 155
+fractal --batch job submit 156 156 156
+fractal --batch job submit 157 157 157
+fractal --batch job submit 158 158 158
+fractal --batch job submit 159 159 159
+fractal --batch job submit 160 160 160
+fractal --batch job submit 161 161 161
+fractal --batch job submit 162 162 162
+fractal --batch job submit 163 163 163
+fractal --batch job submit 164 164 164
