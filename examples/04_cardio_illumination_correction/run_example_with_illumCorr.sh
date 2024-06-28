3bc145781072c77102649887eb8c6800d10f2c0c
Tommaso Comparin <3862206+tcompa@users.noreply.github.com>
2024-06-28 11:43:46 +0200

    Replace several `"True"` with `true` in JSON files

--- examples/04_cardio_illumination_correction/run_example_with_illumCorr.sh
+++ examples/04_cardio_illumination_correction/run_example_with_illumCorr.sh
@@ -45,7 +45,7 @@ echo "WF_ID: $WF_ID"
 # Add tasks to workflow
 fractal --batch workflow add-task $PROJECT_ID $WF_ID --task-name "Create OME-Zarr structure" --args-file Parameters/args_create_ome_zarr_illum_corr.json --meta-file Parameters/example_meta.json
 fractal --batch workflow add-task $PROJECT_ID $WF_ID --task-name "Convert Yokogawa to OME-Zarr"
-echo "{\"overwrite_input\": \"True\", \"illumination_profiles_folder\": \"`pwd`/../illum_corr_images/\", \"dict_corr\": {\"A01_C01\": \"20220621_UZH_manual_illumcorr_40x_A01_C01.png\", \"A01_C02\": \"20220621_UZH_manual_illumcorr_40x_A01_C02.png\", \"A02_C03\": \"20220621_UZH_manual_illumcorr_40x_A02_C03.png\"}}" > Parameters/illumination_correction.json
+echo "{\"overwrite_input\": true, \"illumination_profiles_folder\": \"`pwd`/../illum_corr_images/\", \"dict_corr\": {\"A01_C01\": \"20220621_UZH_manual_illumcorr_40x_A01_C01.png\", \"A01_C02\": \"20220621_UZH_manual_illumcorr_40x_A01_C02.png\", \"A02_C03\": \"20220621_UZH_manual_illumcorr_40x_A02_C03.png\"}}" > Parameters/illumination_correction.json
 fractal --batch workflow add-task $PROJECT_ID $WF_ID --task-name "Illumination correction" --args-file Parameters/illumination_correction.json
 fractal --batch workflow add-task $PROJECT_ID $WF_ID --task-name "Copy OME-Zarr structure" --args-file Parameters/copy_ome_zarr.json
 fractal --batch workflow add-task $PROJECT_ID $WF_ID --task-name "Maximum Intensity Projection"
